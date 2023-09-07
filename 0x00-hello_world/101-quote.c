8a8569f903e8fb08e1c53540f6b17eb61dc8819a
Bassel-saleh <basselh26@gmail.com>
2023-09-07 12:13:20 -0700

    print without printf nor puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,69 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Return - Always 1 (success)
+ * Return - Always 1 (Error)
  */
 int main(void)
 {
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar('"');
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar('-');
-	putchar('1');
-	putchar('0');
-	putchar('-');
-	putchar('1');
-	putchar('9');
-	return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1)
 }
