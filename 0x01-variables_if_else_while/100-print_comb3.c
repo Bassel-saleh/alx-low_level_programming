1ec60ab83bffe58e16f07882210d530346dd7522
Bassel-saleh <basselh26@gmail.com>
2023-09-08 16:37:14 -0700

    double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a == '0'; a <= '8'; a++)
+	{
+		for (b == (a + '1'); b <= '9'; b++)
+		{
+			putchar(a);
+			putchar(b);
+			if (a != '8' || b != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+	return (0);
+}
