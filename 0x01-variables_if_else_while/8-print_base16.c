5eedcce95c16256bc055aed780800a456beb3b22
Bassel-saleh <basselh26@gmail.com>
2023-09-08 14:58:48 -0700

    base 16 nums

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	char n = '0';
+
+	while (n <= '15')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
