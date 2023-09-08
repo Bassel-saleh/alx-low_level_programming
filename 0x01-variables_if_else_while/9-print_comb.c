ef2b430c98ca15ef256ff621e10357ba9ad18403
Bassel-saleh <basselh26@gmail.com>
2023-09-08 16:05:26 -0700

    putchar alot

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-        int n = '0';
+	int n = '0';
 
-        while (n <= '9')
-        {
-                putchar(n);
-                if (n != '9')
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-                n++;
-        }
-        putchar('\n');
-        return (0);
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
