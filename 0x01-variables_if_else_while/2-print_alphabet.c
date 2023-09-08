3205453c074d9bf24377bbed4647a6f6630a9185
Bassel-saleh <basselh26@gmail.com>
2023-09-08 13:49:51 -0700

    ALPHAPET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char l = 'a';
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(l);
+		l++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
