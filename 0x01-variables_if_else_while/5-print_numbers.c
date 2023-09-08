0b8ad888031e9362e18477e903785a146b51d548
Bassel-saleh <basselh26@gmail.com>
2023-09-08 14:39:50 -0700

    decimals

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char n = '0';
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
