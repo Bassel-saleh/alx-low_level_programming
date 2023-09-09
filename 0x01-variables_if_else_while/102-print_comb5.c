c4de478f3d76d066569b77eed1733454493b4388
Bassel-saleh <basselh26@gmail.com>
2023-09-08 17:48:47 -0700

    DD DD

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = (a + 1); b <= '9'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = (b + 1); c <= '9'; c++)
+			for (c = a; c <= '9'; c++)
 			{
-				for (d = (c + 1); d <= '9'; d++)
+				for (d = (a == c) ? (b + 1) : 0; d <= '9'; d++)
 				{
 					putchar(a);
 					putchar(b);
