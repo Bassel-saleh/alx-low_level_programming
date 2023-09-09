9fd51c082b3eee8b1a38a73c374c5078d27cb35c
Bassel-saleh <basselh26@gmail.com>
2023-09-08 18:07:12 -0700

    DD DD

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,7 +24,7 @@ int main(void)
 					putchar(' ');
 					putchar(c);
 					putchar(d);
-					if (!((a == '9' && b == '8') && (c == '9' && d == '9')))
+					if (a != '9' || b != '8' || c != '9' || d != '9')
 					{
 						putchar(',');
 						putchar(' ');
