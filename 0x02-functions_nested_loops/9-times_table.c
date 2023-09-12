dc2102b15aa3a20527856b26f509ae49e27bed24
Bassel-saleh <basselh26@gmail.com>
2023-09-12 16:28:21 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - times table
+ */
+void times_table(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = (a * b);
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			if (c <= '9')
+			{
+				_putchar(c + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else 
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+}
