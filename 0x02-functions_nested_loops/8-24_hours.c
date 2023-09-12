56505e5a608409f1302c564cd51f6a37c3b42f36
Bassel-saleh <basselh26@gmail.com>
2023-09-12 15:29:19 -0700

    every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of the day
+ * Return: Always 0 (success)
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	while (a = "00"; a <= "24"; a++)
+	{
+		while (b = "00"; b <= "60"; b++)
+		{
+			printf(a:b'\n');
+		}
+	}
+	return (0)
+}
