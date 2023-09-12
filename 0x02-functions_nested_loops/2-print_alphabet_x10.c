4bbfccefb0e717036175fa1382102058a7e83616
Bassel-saleh <basselh26@gmail.com>
2023-09-12 12:27:49 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet "10" times
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int x = 0;
+
+	while (x < '9')
+	{
+		for (a = 'a'; a <= 'z';a++)
+		{
+			_putchar(a);
+		}
+	_putchar('\n');
+}
