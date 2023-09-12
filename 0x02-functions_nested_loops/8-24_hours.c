df659c9bca14ecc51b88075674400933c428ce21
Bassel-saleh <basselh26@gmail.com>
2023-09-12 16:03:24 -0700

    every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 		while (b < 60)
 		{
 			_putchar('0' + (a / 10));
-                        _putchar('0' + (a % 10));
-                        _putchar(':');
-                        _putchar('0' + (b / 10));
-                        _putchar('0' + (b % 10));
-                        _putchar('\n');
+			_putchar('0' + (a % 10));
+			_putchar(':');
+			_putchar('0' + (b / 10));
+			_putchar('0' + (b % 10));
+			_putchar('\n');
                         b++;
 			b++;
 		}
