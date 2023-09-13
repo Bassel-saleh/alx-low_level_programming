39ea886c59d879d350bf4fad77f2cf447fd2ebdc
Bassel-saleh <basselh26@gmail.com>
2023-09-12 18:49:28 -0700

    * table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - print the times table for a number
+ * @n: is the number used
+ */
+void print_times_table(int n)
+{
+	int t;
+	int r;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (t = 0; t <= 9; t++)
+		{
+			r = n * t;
+			if (r <= 9 && r != 0)
+			{
+				pritnf(",   %i", r);
+			}
+			else if (r > 9 && r <= 99)
+			{
+				printf(",  %i", r);
+			}
+			else if (r > 99)
+			{
+				printf(", %i", r);
+			}
+			else if (t == 0)
+			{
+				_putchar('0');
+			}
+			else if (r == 0)
+			{
+				printf(",   %i", r);
+			}
+		}
+		_putchar('\n');
+	}
+}
+
