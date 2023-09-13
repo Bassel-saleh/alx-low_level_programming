25d2f8979796092e6454e902d66141ea483f5192
Bassel-saleh <basselh26@gmail.com>
2023-09-12 17:01:02 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,13 +4,13 @@
  */
 void times_table(void)
 {
-	int a = 0;
-	int b = 0;
+	int a;
+	int b;
 	int c;
 
-	while (a <= '9')
+	for (a = '0'; a <= '9'' a++)
 	{
-		while (b <= '9')
+		while (b = '0'; b <= '9'; b++)
 		{
 			if (b == '0')
 			{
@@ -34,9 +34,7 @@ void times_table(void)
 				_putchar(c / 10);
 				_putchar(c % 10);
 			}
-			b++;
 		}
 		_putchar('\n');
-		a++;
 	}
 }
