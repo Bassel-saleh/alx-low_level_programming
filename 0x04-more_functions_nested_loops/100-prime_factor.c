b18f77fe91311055ff878f418883d720f5e0088d
Bassel-saleh <basselh26@gmail.com>
2023-09-14 06:10:00 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	int x = 612852475143;
+
+	for (a = 1; a < x; a++)
+	{
+		if ((x % a) = '0')
+		{
+			b = a;
+			break;
+		}
+	}
+	for (a = 1; a < b; a++)
+	{
+		if ((b % a) = '0')
+		{
+			c = a;
+			break;
+		}
+	}
