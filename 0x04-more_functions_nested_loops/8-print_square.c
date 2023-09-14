5f5673450f0338f3caf2834ac439d92f7f9aa948
Bassel-saleh <basselh26@gmail.com>
2023-09-13 22:40:43 -0700

    #square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print a square using '#'
+ * @size: is the length of its side
+ * Return: void
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size >= 1)
+	{
+		for (a == 1; a <= size; a++)
+		{
+			for (b == 1; b <= size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
