c86605acba91a0d3303c9dda5050bed4efceb896
Bassel-saleh <basselh26@gmail.com>
2023-09-14 03:03:14 -0700

    print most decimals

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@ void print_most_numbers(void)
 {
 	int a = '0';
 
-	while (a <= 9)
+	while (a <= '9')
 	{
-		if (a != 2 && a != 4)
+		if (a != '2' && a != '4')
 		{
 			_putchar(a);
 		}
-		a++
+		a++;
 	}
 	_putchar('\n');
 }
