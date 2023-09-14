3cff4be4d1e711b55e8ed5af939cda86fabb20e7
Bassel-saleh <basselh26@gmail.com>
2023-09-14 05:03:09 -0700

    #square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,9 @@ void print_square(int size)
 
 	if (size >= 1)
 	{
-		for (a == 1; a <= size; a++)
+		for (a = 1; a <= size; a++)
 		{
-			for (b == 1; b <= size; b++)
+			for (b = 1; b <= size; b++)
 			{
 				_putchar('#');
 			}
