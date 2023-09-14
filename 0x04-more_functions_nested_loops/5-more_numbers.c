bd3bc6ac88f95ce8b874cd7c3f1e00ea2bf85f83
Bassel-saleh <basselh26@gmail.com>
2023-09-13 21:36:47 -0700

    0-14x10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14 x10
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a = '0';
+	int b = '0';
+
+	while (b <= 9)
+	{
+		while (a <= 14)
+		{
+			if (a > 9)
+			{
+				_putchar(a / 10);
+			}
+			_putchar(a % 10);
+			a++;
+		}
+		b++;
+	}
+}
