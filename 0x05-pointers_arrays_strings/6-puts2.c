2c8676a91fa2b93673991449f9a0ffa1ac31acea
Bassel-saleh <basselh26@gmail.com>
2023-09-18 09:02:31 -0700

    print odds only

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,15 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	i = 0;
+	while (*str[i] != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		if ((i % 2) != '/0')
+			_putchar(*str[i]);
+
+		i++;
 	}
 	_putchar('\n');
 }
