eeb08980aa0fccb7d166886c27d9f9b540ba391c
Bassel-saleh <basselh26@gmail.com>
2023-09-18 07:25:57 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: is the string to be printed
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		*str++;
+	}
+	_putchar('\n');
+}
