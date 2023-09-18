60fc76891067a63208df689ff71c4e39c76ced6e
Bassel-saleh <basselh26@gmail.com>
2023-09-18 06:22:58 -0700

    length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/**
+/**i
  * _strlen - calculates the length of a string
  * @s: is the character to be checked
  * Return: the length of the string
@@ -9,9 +9,8 @@ int _strlen(char *s)
 	int a;
 
 	a = '1';
-	while (*s != '\0')
+	while (*s[a] != '\0')
 	{
-		*s++;
 		a++;
 	}
 	return (a);
