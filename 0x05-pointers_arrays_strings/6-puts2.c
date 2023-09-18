9f6fec49e79e8fd5bbd5f9442a7fd0acf9111e22
Bassel-saleh <basselh26@gmail.com>
2023-09-18 08:49:43 -0700

    print odds only

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - print odds characters only
+ * @str: is the string to be printed
+ * Return: no return
+ */
+void puts2(char *str)
+{
+	while (s != '\0')
+	{
+		_putchar(s);
+		s+=2;
+	}
+	_putchar('\n');
+}
