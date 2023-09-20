40d1d3dcf2407e77a1f9cff1d0a923ba07c004c7
Bassel-saleh <basselh26@gmail.com>
2023-09-20 00:33:35 -0700

    round 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - append src string to dest string
+ * @src: is the string to be appended onto dest
+ * @dest: is the string appended on
+ * @n: is 
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, h;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++
+	}
+	h = 0;
+	while (src[h] != '\0')
+	{
+		src[h] = dest[i];
+		h++;
+		i++;
+	}
+	return (dest);
+}
