3acbb51a379f0ed2036d92e39da289483c101b97
Bassel-saleh <basselh26@gmail.com>
2023-09-20 05:41:22 -0700

    reverse

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -38,3 +38,4 @@ int main(void)
 	reverse_array(a, sizeof(a) / sizeof(int));
 	print_array(a, sizeof(a) / sizeof(int));
 	return (0);
+}
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -19,3 +19,4 @@ void reverse_array(int *a, int n)
 		i--;
 		j++;
 	}
+}
