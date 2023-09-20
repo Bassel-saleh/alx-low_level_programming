a3427df9ab34494f02194463ccb8fbbcfc158967
Bassel-saleh <basselh26@gmail.com>
2023-09-20 09:22:36 -0700

    print any number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,10 +14,17 @@ void print_number(int n)
 		_putchar('-');
 		x = -n;
 	}
-
+	if (x / 1000 != 0)
+	{
+		_putchar(x / 1000);
+	}
+	if (x / 100 != 0)
+	{
+		_putchar(x / 100);
+	}
 	if (x / 10 != 0)
 	{
-		print_number(x / 10);
+		_putchar(x / 10);
 	}
 	_putchar((x % 10) + '0');
 }
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int p = 0;
+
+	while (str[p] != '\0')
+	{
+		char c = str[p];
+
+		_putchar(c);
+		p++;
+	}
+	_putchar('\n');
+	return (0);
+}
