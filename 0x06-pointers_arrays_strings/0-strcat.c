9f202bd0c7fc5e006b3698b10822f56fecb7893e
Bassel-saleh <basselh26@gmail.com>
2023-09-20 00:20:45 -0700

    add a string to the end of another

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,21 +3,23 @@
  * _strcat - append src string to dest string
  * @src: is the string to be appended onto dest
  * @dest: is the string appended on
- * Return:
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int i, h;
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++
 	}
-	for (h = 0; src[h] <= '\0';h++)
+	h = 0;
+	while (src[h] != '\0')
 	{
-		dest[i] = src[h];
+		src[h] = dest[i];
+		h++;
 		i++;
 	}
-	return (0);
+	return (dest);
 }
