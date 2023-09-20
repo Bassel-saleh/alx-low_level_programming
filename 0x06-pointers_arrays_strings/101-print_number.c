4789c51207f40ae0acaf06bfaa105fcdc9abf9c1
Bassel-saleh <basselh26@gmail.com>
2023-09-20 08:59:13 -0700

    print any number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_number - print any number
+  * @n: is the number to be printed
+  * Return: 0
+ **/
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -n;
+	}
+
+	if (x / 10 != 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
