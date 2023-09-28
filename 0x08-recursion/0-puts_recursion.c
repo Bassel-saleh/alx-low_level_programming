23f603839036fbcdea4b0b313361a91d5da41e38
Bassel-saleh <basselh26@gmail.com>
2023-09-28 11:20:50 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - 
+ * @s: 
+ * Return: 
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return(0);
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+int main()
+{
+	_puts_recursion(*s);
+	return (0);
+}
