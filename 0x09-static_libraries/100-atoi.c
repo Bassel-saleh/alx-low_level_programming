c533e87f2e146eb0fd1949f2ad8d09abd7a3f28b
Bassel-saleh <basselh26@gmail.com>
2023-10-02 11:46:13 -0700

    creating static lib

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -6,4 +6,21 @@
  */
 int _atoi(char *s)
 {
+	int si = 1;
+	unsigned int n = 0;
+
+	do
+	{
+
+		if (*s == '-')
+			si *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+
+		else if (n > 0)
+			break;
+	}
+	while (*s++);
+	return (n * si);
 }
