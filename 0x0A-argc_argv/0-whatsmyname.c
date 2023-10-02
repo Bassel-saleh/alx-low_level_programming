890d99dd45f128e92440bec999112141f383c607
Bassel-saleh <basselh26@gmail.com>
2023-10-02 16:07:44 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print file name
+ * @argc: arguments count
+ * @argv: arguments array
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
