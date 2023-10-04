87874266a043b5808e265c31dcc187f4eaecee45
Bassel-saleh <basselh26@gmail.com>
2023-10-04 05:06:24 -0700

    concentrate arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac:
+ * @av:
+ * Return:
+ */
+char *argstostr(int ac, char **av)
+{
+	int l, i, index, tl;
+	char *result;
+
+	if (ac == 0 || av == 0)
+		return (0);
+	tl = 0;
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != 0)
+		{
+			l = 0;
+			while (av[i][l] != '\0')
+			{
+				l++;
+			}
+			tl += l + 1;
+		}
+	}
+	result = (char *)malloc((tl + 1) * sizeof(char));
+	if (result == 0)
+		return (0);
+	index = 0;
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != 0)
+		{
+			l = 0;
+			while (av[i][l] != '\0')
+			{
+				result[index++] = av[i][l++];
+			}
+			result[index++] = '\n';
+		}
+	}
+	result[index] = '\0';
+	return (result);
+}
