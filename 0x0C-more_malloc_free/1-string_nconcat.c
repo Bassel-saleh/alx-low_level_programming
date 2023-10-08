e62203b9b927833e24e5144ceacc2b716537a6b9
Bassel-saleh <basselh26@gmail.com>
2023-10-08 02:04:58 -0700

    concentrate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (0);
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
-	for (i = 0; s2[i] && i < len2; i++)
+	for (i = 0; s2[i] && i < n; i++)
 		result[len1 + i] = s2[i];
 	result[len1 + len2] = '\0';
 	return (result);
