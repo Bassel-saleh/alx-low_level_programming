dde0da4b98d04dccb41e24f2d0faf571428c0732
Bassel-saleh <basselh26@gmail.com>
2023-10-10 15:57:35 -0700

    new dog info

--- 0x0E-structures_typedef/4-main.c
+++ 0x0E-structures_typedef/4-main.c
@@ -11,6 +11,6 @@ int main(void)
 	dog_t *my_dog;
 
 	my_dog = new_dog(NULL, 0, "Bob");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	printf("My name is %s, my owner is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->owner, my_dog->age);
 	return (0);
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ int _strlen(char *str)
 
 	for (len = 0; *(str + len); len++)
 		;
-	return (0);
+	return (len);
 }
 
 /**
