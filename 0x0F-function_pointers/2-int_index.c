df48f9ccf0e8e5664f48836782d9efbe0eb6171f
Bassel-saleh <basselh26@gmail.com>
2023-10-11 12:18:54 -0700

    search for int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,11 +10,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == 0 || cmp == 0 || size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
+		if (cmp(*(array + i)))
 			return (i);
 	}
 	return (-1);
