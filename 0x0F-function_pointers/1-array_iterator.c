d4086a751bff18c8e8d6f731cf0c96b68183d8d8
Bassel-saleh <basselh26@gmail.com>
2023-10-11 11:52:17 -0700

    function executer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,6 +9,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array != 0 && action != 0)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
