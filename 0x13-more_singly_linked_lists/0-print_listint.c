c63ca7fdd1dbeb9f9fbbb3ff38de395184e5d683
Bassel-saleh <basselh26@gmai.com>
2023-10-23 23:48:27 +0300

    added task#0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - print int linked list
+ * @h: the list to be printed
+ * Return: number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
