c2bd201d3d4c793adf892404ec2449aa8f3eaa1f
Bassel-saleh <basselh26@gmai.com>
2023-10-24 16:19:54 +0300

    sm fixes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (i = 0; i < index && head != 0; i++)
+	for (i = 0; i < index && head != 0; i++)
 		head = head->next;
 	return (head);
 }
