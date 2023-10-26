e78494668e2c2007c6ce293a5506900f0fec1e59
Bassel-saleh <basselh26@gmai.com>
2023-10-26 13:32:43 +0300

    added task#6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *endian_check = (char *)&num;
+
+	if (*endian_check == 1)
+		return (1);
+	else
+		return 0;
+}
