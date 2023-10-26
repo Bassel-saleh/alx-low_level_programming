9cde6ff51d221a7641709055a9e205d2c54867df
Bassel-saleh <basselh26@gmai.com>
2023-10-26 13:07:35 +0300

    betty compliant

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,6 +14,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	mask = 1UL << index;
-	bit =(n & mask) ? 1 : 0;
+	bit = (n & mask) ? 1 : 0;
 	return (bit);
 }
