ef8b12462354a9a7227c44682c623a1aae00c253
Bassel-saleh <basselh26@gmai.com>
2023-10-30 17:39:37 +0200

    sm fixes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!(filename))
 		return (-1);
-	m_file = open(filename, O_WRONLY, O_APPEND);
+	m_file = open(filename, O_WRONLY | O_APPEND);
 	if (m_file == -1)
 		return (-1);
 	if (text_content)
