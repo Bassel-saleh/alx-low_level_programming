984cab8ef7af2a06c15bdd0345daf6de913fed84
Bassel-saleh <basselh26@gmai.com>
2023-12-23 01:04:48 +0200

    debugging

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  * @size: size of hash table
  * Return: index (key)
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
