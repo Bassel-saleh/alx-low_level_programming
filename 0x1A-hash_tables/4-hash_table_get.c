c57a5c63e420d537b8be02b27f9e93dc4daa7a4d
Bassel-saleh <basselh26@gmai.com>
2023-12-23 02:22:53 +0200

    debugging

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 			key == NULL || strlen(key) == 0)
 		return (NULL);
-	i = key_index((const unsigned char *)key, ht-size);
+	i = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[i];
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
