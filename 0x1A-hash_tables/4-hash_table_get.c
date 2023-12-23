45b5790e801e466957dacd0465470cd213237131
Bassel-saleh <basselh26@gmai.com>
2023-12-23 02:16:31 +0200

    added task#4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get -  retrieves a value associated with a key
+ * @ht: the hash table
+ * @key: the key
+ * Return: NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+			key == NULL || strlen(key) == 0)
+		return (NULL);
+	i = key_index((const unsigned char *)key, ht-size);
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
